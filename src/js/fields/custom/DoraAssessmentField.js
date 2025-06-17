12f5114beb00dd2b756a8a5bb2d6c768bdb58d5b
Ragnar Stølsmark <ragnar@identitystream.com>
2025-06-17 10:57:40 +0200

    Added dora assessment field

---
+++ src/js/fields/custom/DoraAssessmentField.js
@@ -0,0 +1,97 @@
+(function($) {
+
+    var Alpaca = $.alpaca;
+
+    Alpaca.Fields.DoraAssessmentField = Alpaca.Fields.RadioField.extend({
+        setup: function()
+        {
+            this.base();
+            //need to do this after all fields have been rendered
+            setTimeout(this.setupDoraFields.bind(this), 0);
+        },
+
+        getFieldType: function () {
+            return "DoraAssessment";
+        },
+
+        getTitle: function () {
+            return "Dora assessment";
+        },
+
+        getDescription: function () {
+            return "Renders a dora assessment field.";
+        },
+
+        setupDoraFields: function (){
+            var self = this
+            var doraFields = [
+                "DoraAssessmentIsACriticalServiceAffected",
+                "DoraAssessmentUnauthorizedAccess",
+                "DoraAssessmentClients",
+                "DoraAssessmentDataLoss",
+                "DoraAssessmentReputation",
+                "DoraAssessmentDuration",
+                "DoraAssessmentGeographicalSpread",
+                "DoraAssessmentAffectedCountries",
+                "DoraAssessmentEconomicImpact"
+            ];
+            doraFields.forEach(function (doraField){
+                var doraAlpacaField = self.parent.childrenByPropertyId[doraField];
+                if (doraAlpacaField) {
+                     doraAlpacaField.getFieldEl().bind("fieldupdate", function(event)
+                        {
+                            self.updateDoraAssessment();
+                        }
+                    );
+                }
+            });
+            self.updateDoraAssessment();
+        },
+        
+        updateDoraAssessment: function() {
+            var self = this;
+            var currentValue = self.getValue();
+            console.log("Doing dora assesment.");
+            var doraAssessment = "No";
+            if(self.parent.childrenByPropertyId["DoraAssessmentIsACriticalServiceAffected"].getValue() !== "Yes") {
+                if (doraAssessment !== currentValue) {
+                    self.setValue(doraAssessment);
+                }
+                return;
+            }
+
+            if(self.parent.childrenByPropertyId["DoraAssessmentUnauthorizedAccess"].getValue() === "Yes") {
+                doraAssessment = "Yes";
+                if (doraAssessment !== currentValue) {
+                    self.setValue(doraAssessment);
+                }
+                return;
+            }
+            var additionalCriteria =  [
+                "DoraAssessmentClients",
+                "DoraAssessmentDataLoss",
+                "DoraAssessmentReputation",
+                "DoraAssessmentDuration",
+                "DoraAssessmentGeographicalSpread",
+                "DoraAssessmentEconomicImpact"
+            ];
+            var numAdditionalCriteria = 0;
+            additionalCriteria.forEach(function (additionalCriterion){
+                 if(self.parent.childrenByPropertyId[additionalCriterion].getValue() === "Yes") {
+                    numAdditionalCriteria++;
+                 }
+            });
+
+            if(numAdditionalCriteria > 1){
+                doraAssessment = "Yes";
+            }
+            if(doraAssessment !== currentValue) {
+                self.setValue(doraAssessment);
+            }
+
+        }
+    });
+
+    Alpaca.registerFieldClass("DoraAssessment", Alpaca.Fields.DoraAssessmentField);
+
+})(jQuery);
