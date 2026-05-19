6085333d2c877b75bf565e1c63daf783c68c4574
Nikolai Norum Vinningland <nikolai@identitystream.com>
2026-05-19 13:04:49 +0200

    Fix select2 and Select2EntityPicker disabling

--- src/js/fields/list/Select2Field.js
+++ src/js/fields/list/Select2Field.js
@@ -177,6 +177,20 @@
             }
         },
 
+        disable: function() {
+            var $el = $(this.getControlEl());
+            if ($el.data('select2')) {
+                $el.prop("disabled", true);
+            }
+        },
+
+        enable: function() {
+            var $el = $(this.getControlEl());
+            if ($el.data('select2')) {
+                $el.prop("disabled", false);
+            }
+        },
+
         _validateEnum: function () {
             return true;
         },
